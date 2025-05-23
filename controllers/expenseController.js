19e15c1807e921293b34c60e7414c1cdd1772ecb
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-23 09:43:54 +0530

    crud with transaction

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -3,21 +3,32 @@ const Expense=require('../models/expenseModel');
 const sequelize = require('../utils/db-connection');
 
 const addExpense=async(req,res)=>{
+    const transaction=await sequelize.transaction();
     try {
+        
        const {price,description,category}=req.body;
        console.log(req.user);
 const newExpese=await Expense.create({
     price,description,category,UserId:(req.user.id)
-})
-const existingUser=await User.findByPk(req.user.id);
+},
+{transaction:transaction}
+);
+// throw new Error();
+const existingUser=await User.findByPk(req.user.id,
+    {transaction:transaction}
+);
 if(existingUser){
     existingUser.totalExpense = (existingUser.totalExpense || 0) + Number(price);
-    await existingUser.save();
+    await existingUser.save(
+        {transaction:transaction}
+    );
 }else{
     console.log("in addExpense User not found")
 }
+await transaction.commit();
 res.status(201).json({message:"expense created successfully", expense:newExpese}); 
     } catch (error) {
+        await transaction.rollback();
         console.log(error);
         res.status(500).json({message:error.message});
     }
@@ -34,9 +45,23 @@ res.status(200).json({message:"got all expenses successfully",expenses});
 
 }
 const deleteExpense=async(req,res)=>{
+    const transaction=await sequelize.transaction();
     try {
         const {id}=req.params;
-        const deleted=await Expense.destroy({where:{id}});
+        const expense=await Expense.findByPk(id,{transaction:transaction});
+
+        const existingUser=await User.findByPk(req.user.id,
+    {transaction:transaction}
+);
+if(existingUser && expense){
+    existingUser.totalExpense = (existingUser.totalExpense || 0) - expense.price;
+    await existingUser.save(
+        {transaction:transaction}
+    );
+}else{
+    console.log("User not found")
+}
+        const deleted=await Expense.destroy({where:{id}},{transaction:transaction});
         console.log(id);
         console.log(deleted);
         if(deleted){
@@ -44,7 +69,9 @@ const deleteExpense=async(req,res)=>{
         }else{
             res.status(404).json({message:'Expense not found'});
         }
+        await transaction.commit();
     } catch (error) {
+        await transaction.rollback();
          console.log(error);
         res.status(500).json({message:error.message});
     }
